bf15cd8c7c2061f2f2d1cd47eb9905cddb311b68
Ali Yousefi <38606021+u3fiii@users.noreply.github.com>
2024-11-29 21:23:37 +0330

    feat: projects width resolved

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -27,40 +27,6 @@ const Projects: React.FC = () => {
     fetchProjects(); // Call the fetch function
   }, []);
 
-  // Dragging functionality
-  const handleMouseDown = (e: React.MouseEvent) => {
-    if (containerRef.current) {
-      // Use a conditional check to ensure containerRef.current is not null
-      containerRef.current.style.cursor = "grabbing"; // Change cursor to grabbing
-      const startX = e.pageX - containerRef.current.offsetLeft; // Get the starting X position
-      const scrollLeft = containerRef.current.scrollLeft; // Get the current scroll position
-
-      const handleMouseMove = (moveEvent: MouseEvent) => {
-        if (containerRef.current) {
-          // Use a conditional check inside the nested function
-          const x = moveEvent.pageX - containerRef.current.offsetLeft; // Get the current X position
-          const walk = (x - startX) * 2; // Calculate the distance to scroll
-          containerRef.current.scrollLeft = scrollLeft - walk; // Update scroll position
-        }
-      };
-
-      const handleMouseUp = () => {
-        if (containerRef.current) {
-          // Use a conditional check inside the nested function
-          containerRef.current.style.cursor = "grab"; // Reset cursor
-          containerRef.current.removeEventListener(
-            "mousemove",
-            handleMouseMove
-          ); // Remove event listener
-          containerRef.current.removeEventListener("mouseup", handleMouseUp); // Remove event listener
-        }
-      };
-
-      containerRef.current.addEventListener("mousemove", handleMouseMove); // Add event listener for mouse move
-      containerRef.current.addEventListener("mouseup", handleMouseUp); // Add event listener for mouse up
-    }
-  };
-
   if (loading) {
     return <div>Loading...</div>; // Show loading state
   }
@@ -68,11 +34,7 @@ const Projects: React.FC = () => {
   return (
     <div className="projects" id="Projects">
       <h1 className="projects-title">Projects</h1>
-      <div
-        className="projects-container"
-        ref={containerRef}
-        onMouseDown={handleMouseDown} // Add mouse down event
-      >
+      <div className="projects-container" ref={containerRef}>
         {projects.map((project) => (
           <ProjectItem
             key={project.sys.id}
--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,84 +1,53 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import designIcon from "../../assets/design.svg";
-import developmentIcon from "../../assets/development.svg";
-import motionIcon from "../../assets/motion.svg";
+import designIcon from "../../assets/skills/design.svg";
+import developmentIcon from "../../assets/skills/develop.svg";
+import motionIcon from "../../assets/skills/motion.svg";
 
-import {
-  faFigma,
-  faReact,
-  faHtml5,
-  faCss3Alt,
-  faJs,
-  faFacebook, // Using faFilm as a placeholder for Adobe After Effects
-  faAirbnb,
-  faAmazon,
-  faMicrosoft,
-  faTwitch,
-} from "@fortawesome/free-brands-svg-icons";
 import "../../styles/skills/skills.scss";
-
-const skillsData = {
-  "Product Design": [
-    { name: "Figma", icon: faFigma },
-    { name: "Sketch", icon: faFigma }, // Placeholder icon
-    { name: "Adobe XD", icon: faFigma }, // Placeholder icon
-    { name: "InVision", icon: faFigma }, // Placeholder icon
-    { name: "User Research", icon: faFacebook }, // Placeholder icon
-  ],
-  Development: [
-    { name: "React", icon: faReact },
-    { name: "HTML5", icon: faHtml5 },
-    { name: "CSS3", icon: faCss3Alt },
-    { name: "JavaScript", icon: faJs },
-    { name: "Mobile Development", icon: faAirbnb },
-  ],
-  "Motion Design": [
-    { name: "Adobe After Effects", icon: faAmazon }, // Using faFilm as a placeholder
-    { name: "Principle", icon: faFacebook }, // Using faFilm as a placeholder
-    { name: "Framer", icon: faMicrosoft }, // Using faFilm as a placeholder
-    { name: "Lottie", icon: faTwitch }, // Using faFilm as a placeholder
-    { name: "Animation", icon: faFacebook }, // Using faPaintBrush as a placeholder
-  ],
-};
-
-// Define the category icons
-const categoryIcons = {
-  "Product Design": designIcon,
-  Development: developmentIcon,
-  "Motion Design": motionIcon,
-};
-
-// Define a type for the categories
-type SkillCategory = keyof typeof skillsData;
 
 const Skills = () => {
   return (
     <div className="skills" id="Skills">
-      <h2>My Skills</h2>
-      <div className="category-wrapper">
-        {Object.entries(skillsData).map(([category, skills]) => {
-          const typedCategory = category as SkillCategory; // Type assertion here
-          return (
-            <div key={category} className="skills-category">
-              <h3>
-                <img
-                  src={categoryIcons[typedCategory]} // Use typedCategory here
-                  alt={`${category} icon`}
-                  className="category-icon"
-                />
-                {category}
-              </h3>
-              <ul className="skills-list">
-                {skills.map((skill) => (
-                  <li key={skill.name} className="skill-item">
-                    <FontAwesomeIcon icon={skill.icon} className="skill-icon" />
-                    <span>{skill.name}</span>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          );
-        })}
+      <h1>My Skills</h1>
+      <div className="skills-container">
+        <div className="category-skill design">
+          <div className="category-title">
+            <img src={designIcon} />
+            <h3>Design</h3>
+          </div>
+          <div className="skills-items">
+            <p>
+              User Interface / User Research / Usability Testing / Data Analysis{" "}
+            </p>
+          </div>
+        </div>
+        <div className="category-skill develop">
+          <div className="category-title">
+            <img src={developmentIcon} />
+            <h3>Develop</h3>
+          </div>
+          <div
+            className="skills-items
+          "
+          >
+            <p>
+              User Interface / User Research / Usability Testing / Data Analysis{" "}
+            </p>
+          </div>
+        </div>
+        <div className="category-skill motion">
+          <div className="category-title">
+            <img src={motionIcon} />
+            <h3>Motion</h3>
+          </div>
+          <div
+            className="skills-items
+          "
+          >
+            <p>
+              User Interface / User Research / Usability Testing / Data Analysis{" "}
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
